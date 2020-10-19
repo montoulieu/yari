a70fdde960a9e3866bbeb4a120fd76ef54c34b58
Peter Bengtsson <mail@peterbe.com>
2020-10-19 14:32:54 -0400

    not lazy-load banner CSS maybe

--- client/src/banners/active-banner.tsx
+++ client/src/banners/active-banner.tsx
@@ -22,11 +22,11 @@ import { CATEGORY_MONTHLY_PAYMENTS, useGA } from "../ga-context";
 import { useLocale } from "../hooks";
 import { useUserData } from "../user-context";
 
-// TODO: split up banners into separate lazy-loaded component files
-import "../kumastyles/components/banners/base.scss";
-import "../kumastyles/components/banners/developer-needs.scss";
-import "../kumastyles/components/banners/l10n-survey.scss";
-import "../kumastyles/components/banners/mdn-subscriptions.scss";
+// // TODO: split up banners into separate lazy-loaded component files
+// import "../kumastyles/components/banners/base.scss";
+// import "../kumastyles/components/banners/developer-needs.scss";
+// import "../kumastyles/components/banners/l10n-survey.scss";
+// import "../kumastyles/components/banners/mdn-subscriptions.scss";
 
 // Set a localStorage key with a timestamp the specified number of
 // days into the future. When the user dismisses a banner we use this
